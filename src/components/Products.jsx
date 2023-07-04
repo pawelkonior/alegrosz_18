6ce105fd2cdd6e02d6b40d30a7de88096198f35e
Pawel Konior <pawel.konior@gmail.com>
2023-07-04 14:55:29 +0200

    json server, product list component

---
+++ src/components/Products.jsx
@@ -0,0 +1,32 @@
+import {useEffect, useState} from "react";
+
+function Products() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const controller = new AbortController();
+        getData(controller.signal).then((data) => setProducts(data));
+
+        return () => {
+            controller.abort();
+        }
+    }, [])
+
+    function getData(signal) {
+        return fetch('http://localhost:3000/products', {signal})
+            .then((response) => response.json())
+    }
+
+    return (
+        <>
+            {products.map((product) => (
+                <div key={product.id} style={{border: "1px solid black", padding: 10, margin: 10}}>
+                    <h2>{product.name}</h2>
+                    <p>{product.price}</p>
+                </div>
+            ))}
+        </>
+    )
+}
+
+export default Products;
