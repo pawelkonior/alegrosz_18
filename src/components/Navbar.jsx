484fac78babe88a8cdcfd7a1efabeb9e4ba5b1f8
Pawel Konior <pawel.konior@gmail.com>
2023-07-05 16:58:55 +0200

    mui for material design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,43 @@
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Link } from "react-router-dom";
+
 function Navbar() {
-    return <div>Menu</div>;
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{ flexGrow: 1 }}
+                    >
+                        Alegrosz
+                    </Typography>
+                    <Button color="inherit">
+                        <Link to="/">Products</Link>
+                    </Button>
+                    <Button color="inherit">
+                        <Link to="add-product">Add Product</Link>
+                    </Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
 }
 
 export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import "./index.scss";
 
 import router from "./router.jsx";
+import { CssBaseline } from "@mui/material";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
+        <CssBaseline />
         <RouterProvider router={router} />
     </React.StrictMode>
 );
